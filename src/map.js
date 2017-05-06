ac2a943170dd6da1ccc242eb22d20eab27b033e9
Lucas Manning <lucas.manning21@gmail.com>
2017-05-06 17:18:57 -0400

    added unresponsive colorscale

--- src/map.js
+++ src/map.js
@@ -68,10 +68,48 @@ var svg = d3.select("svg"),
 
 var div = d3.select("body").append("div")   
     .attr("class", "tip")               
-
+//TODO make this responsive
 var key = svg.append("g")
              .attr("class", "key")
              .attr("transform", "translate(0,40)");
+
+var color = d3.scaleThreshold()
+    .domain(d3.range(0, 5))
+    .range(d3.schemeOrRd[6]);
+
+var x = d3.scaleLinear()
+    .domain([0, 5])
+    .rangeRound([600, 860]);
+
+key.selectAll("rect")
+  .data(color.range().map(function(d) {
+      d = color.invertExtent(d);
+      if (d[0] == null) d[0] = x.domain()[0];
+      if (d[1] == null) d[1] = x.domain()[1];
+      return d;
+    }))
+  .enter().append("rect")
+    .attr("height", 8)
+    .attr("x", function(d) { return x(d[0]); })
+    .attr("width", function(d) { return x(d[1]) - x(d[0]); })
+    .attr("fill", function(d) { return color(d[0]); });
+
+
+key.append("text")
+    .attr("class", "caption")
+    .attr("x", x.range()[0])
+    .attr("y", -6)
+    .attr("fill", "#000")
+    .attr("text-anchor", "start")
+    .attr("font-weight", "bold")
+    .text("Disruption rate");
+
+key.call(d3.axisBottom(x)
+    .tickSize(13)
+    .tickFormat(function(x, i) { return i ? x : x + "%"; })
+    .tickValues(color.domain()))
+  .select(".domain")
+    .remove();
 // Helpers======================================================
 
 function toggleMap(mapName) {
